dfc45273839b9ab16ac37c7e6c8aa91bf03eb5fb
BogdanAmariutei <84973862+BogdanAmariutei@users.noreply.github.com>
2022-06-14 11:07:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import os
-import jsonpickle
+#import jsonpickle
 import numpy as np
 from flask import Flask, make_response, request, jsonify, render_template
 from sklearn.metrics import roc_auc_score
 from tensorflow import keras
 from werkzeug.utils import secure_filename
+import pickle
+import json
 
 app = Flask(__name__)
 
@@ -30,7 +32,15 @@ def predict():
 
     output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text='Salary is {}'.format(output))
+    return render_template('index.html', prediction_text='Prediction:'.format(output))
+
+
+def serialize_ndarray(obj):
+    if isinstance(obj, np.ndarray):
+        return obj.tolist()
+    raise TypeError ("Type %s is not serializable" % type(obj))
+
+
 
 
 @app.route('/predict_api',methods=['POST'])
@@ -39,10 +49,13 @@ def predict_api():
     filename = secure_filename(signal.filename)
     path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
     signal.save(path)
-
-
+    semnal = pickle.load(open('semnal.pkl', 'rb'))
+    pred = pickle.load(open('pred.pkl', 'rb'))
+    jsonPred = json.dumps(pred, default = serialize_ndarray)
+    jsonSemnal = json.dumps(semnal, default = serialize_ndarray)
     # resp = make_response(jsonpickle.encode((x, y), unpicklable=False), 200)
-    resp = make_response("test", 200)
+    resp = make_response(jsonPred, 200)
+    resp = make_response(jsonSemnal, 200)
     return resp
 
 if __name__ == "__main__":
